7a1b407d8d2a5f3a98c896e1f85ee7d61dcf9344
Opall87 <79499607+Opall87@users.noreply.github.com>
2021-03-02 05:46:10 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Opall*
+    
+      ${prefix}setprefix
+      ${prefix}block
+      ${prefix}bc
+      ${prefix}bcgc
+      ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
